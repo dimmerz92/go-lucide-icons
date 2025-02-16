511e1c8985cb7834e911ec136731ec767a80c1bd
Andrew Weymes <andrew.weymes@sittellalab.com.au>
2025-02-16 19:14:42 +0800

    update tests

--- internal/all_test.go
+++ internal/all_test.go
@@ -2,26 +2,33 @@ package internal
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 )
 
 func TestGetNewFiles(t *testing.T) {
 	src := "./tests/src"
-	target := "./tests/target"
+	htmlTarget := "./tests/html_target"
+	templTarget := "./tests/templ_target"
 	newFiles := []string{"tests/src/newfile1.svg", "tests/src/newfile2.svg"}
 
-	files, err := getNewFiles(src, target)
+	output, err := getNewFiles(src, htmlTarget, templTarget)
 	if err != nil {
 		t.Fatalf("failed to get new files: %v", err)
 	}
 
-	if len(newFiles) != len(files) {
-		t.Fatalf("getNewFiles returned %v\twant %v", files, newFiles)
+	if len(newFiles) != len(output["html"]) {
+		t.Fatalf("getNewFiles returned %v\twant %v", output["html"], newFiles)
+	}
+	if len(newFiles) != len(output["templ"]) {
+		t.Fatalf("getNewFiles returned %v\twant %v", output["templ"], newFiles)
 	}
 
-	for i := range files {
-		if files[i] != newFiles[i] {
-			t.Fatalf("%s should not be in returned array", files[i])
+	for _, fileType := range output {
+		for i := range fileType {
+			if fileType[i] != newFiles[i] {
+				t.Fatalf("%s should not be in returned array", fileType[i])
+			}
 		}
 	}
 }
@@ -43,7 +50,7 @@ func TestKebabtoPascalCase(t *testing.T) {
 
 func TestGenerateTemplIcon(t *testing.T) {
 	file := "./tests/src/newfile1.svg"
-	target := "./tests/target/"
+	target := "tests/templ_target"
 	cwd, err := os.Getwd()
 	if err != nil {
 		t.Fatalf("failed to get current working directory: %v", err)
@@ -53,25 +60,27 @@ func TestGenerateTemplIcon(t *testing.T) {
 		t.Fatalf("failed to generate templ icon: %v", err)
 	}
 
-	if _, err := os.Stat(cwd + "/" + target + "newfile1.templ"); err != nil {
+	_, err = os.Stat(filepath.Join(cwd, target, "newfile1.templ"))
+	if err != nil {
 		t.Fatalf("templ icon did not get created: %v", err)
 	}
 
-	newFile, err := os.ReadFile(target + "newfile1.templ")
+	newFile, err := os.ReadFile(filepath.Join(target, "newfile1.templ"))
 	if err != nil {
 		t.Fatalf("failed to read newly generated templ file: %v", err)
 	}
 
 	t.Logf("inspect generated file contents:\n%s", string(newFile))
 
-	if err := os.Remove(cwd + "/" + target + "newfile1.templ"); err != nil {
+	err = os.Remove(filepath.Join(cwd, target, "newfile1.templ"))
+	if err != nil {
 		t.Logf("failed to remove the test generated templ file: %v", err)
 	}
 }
 
 func TestGenerateHtmlIcon(t *testing.T) {
 	file := "./tests/src/newfile1.svg"
-	target := "./tests/target/"
+	target := "tests/html_target"
 	cwd, err := os.Getwd()
 	if err != nil {
 		t.Fatalf("failed to get current working directory: %v", err)
@@ -81,18 +90,20 @@ func TestGenerateHtmlIcon(t *testing.T) {
 		t.Fatalf("failed to generate templ icon: %v", err)
 	}
 
-	if _, err := os.Stat(cwd + "/" + target + "newfile1.html"); err != nil {
+	_, err = os.Stat(filepath.Join(cwd, target, "newfile1.html"))
+	if err != nil {
 		t.Fatalf("html icon did not get created: %v", err)
 	}
 
-	newFile, err := os.ReadFile(target + "newfile1.html")
+	newFile, err := os.ReadFile(filepath.Join(target, "newfile1.html"))
 	if err != nil {
 		t.Fatalf("failed to read newly generated html file: %v", err)
 	}
 
 	t.Logf("inspect generated file contents:\n%s", string(newFile))
 
-	if err := os.Remove(cwd + "/" + target + "newfile1.html"); err != nil {
+	err = os.Remove(filepath.Join(cwd, target, "newfile1.html"))
+	if err != nil {
 		t.Logf("failed to remove the test generated html file: %v", err)
 	}
 }
