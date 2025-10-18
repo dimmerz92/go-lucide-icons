422596e6fd18e19e570cb8d7347bed4f3d9b1c16
Andrew Weymes <andrew.weymes@sittellalab.com.au>
2025-10-18 23:58:54 +0800

    test template embedding

--- html-icons/embed.go
+++ html-icons/embed.go
@@ -2,6 +2,7 @@ package htmlicons
 
 import (
 	"embed"
+	"html/template"
 	"io/fs"
 	"path"
 )
@@ -11,4 +12,10 @@ var templates embed.FS
 
 func GetHtmlFile(name string) ([]byte, error) {
 	return fs.ReadFile(templates, path.Join("templates", name+".html"))
+}
+
+// AddLucideIcons combines the lucide icons with the given template.
+func AddLucideIcons(tpls *template.Template) error {
+	_, err := tpls.ParseFS(templates, "templates/*.html")
+	return err
 }
--- html-icons/embed_test.go
+++ html-icons/embed_test.go
@@ -1,6 +1,8 @@
 package htmlicons_test
 
 import (
+	"html/template"
+	"strings"
 	"testing"
 
 	htmlicons "github.com/dimmerz92/go-lucide-icons/html-icons"
@@ -16,4 +18,92 @@ func TestGetHtmlFile(t *testing.T) {
 	if len(data) == 0 {
 		t.Fatal("returned empty data")
 	}
+}
+
+func TestAddLucideIcons(t *testing.T) {
+	html := `{{ define "icon" }}<svg>...</svg>{{ end }}`
+	tpls := template.New("base")
+
+	_, err := tpls.Parse(html)
+	if err != nil {
+		t.Fatalf("failed to parse: %v", err)
+	}
+
+	err = htmlicons.AddLucideIcons(tpls)
+	if err != nil {
+		t.Fatalf("failed to add icons: %v", err)
+	}
+
+	t.Run("test existence", func(t *testing.T) {
+		if tpls.Lookup("a-arrow-down") == nil {
+			t.Fatal("expected icon to be present, got none")
+		}
+	})
+
+	t.Run("test without args", func(t *testing.T) {
+		want := `
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  
+>
+  <path d="m14 12 4 4 4-4" />
+  <path d="M18 16V7" />
+  <path d="m2 16 4.039-9.69a.5.5 0 0 1 .923 0L11 16" />
+  <path d="M3.304 13h6.392" />
+</svg>
+`
+		var b strings.Builder
+		err := tpls.ExecuteTemplate(&b, "a-arrow-down", nil)
+		if err != nil {
+			t.Fatalf("failed to execute: %v", err)
+		}
+
+		got := b.String()
+		if got != want {
+			t.Fatalf("got\n%s\n\nwanted\n%s", got, want)
+		}
+	})
+
+	t.Run("test with args", func(t *testing.T) {
+		want := `
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  
+    id="my-icon"
+  
+>
+  <path d="m14 12 4 4 4-4" />
+  <path d="M18 16V7" />
+  <path d="m2 16 4.039-9.69a.5.5 0 0 1 .923 0L11 16" />
+  <path d="M3.304 13h6.392" />
+</svg>
+`
+		var b strings.Builder
+		err := tpls.ExecuteTemplate(&b, "a-arrow-down", []template.HTMLAttr{`id="my-icon"`})
+		if err != nil {
+			t.Fatalf("failed to execute: %v", err)
+		}
+
+		got := b.String()
+		if got != want {
+			t.Fatalf("got\n%s\n\nwanted\n%s", got, want)
+		}
+	})
+
 }
